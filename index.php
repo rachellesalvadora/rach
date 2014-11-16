5323cf6e4fcf9c8c966421dcb81bc52f5fbc1f27
rachelle <rachellesalvadora@gmail.com>
2014-11-16 22:39:05 +1100

    index.php head added

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0">
+
+	<title>Rachelle Salvadora</title> 
+	<link rel="stylesheet" href="css/style.css">
+</head>
