ff8fea222b80c309b9ae062458779ba732bfda20
rachelle <rachellesalvadora@gmail.com>
2014-11-17 09:05:07 +1100

    my partner added

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 			</p>
 			<p class="grid__item two-thirds">CS50 Was an Introduction to Computer Science MOOC offered through Harvard University through edX. The course was designed to teach studnets how to think algorithmically and solve problems efficiently. <br/><br/>
 				There were multiple ways to take the course,
-				I chose to take the one straight from Harvard University with a verified certificate, Senior lecturer on Computer Science of Harvard University, <a href="http://cs.harvard.edu/malan">David Malan</a>'s Signature and Huntington D. Lambert, Dean of Harvard Extension School.<br/>
+				I chose to take the one with a verified certificate, Senior lecturer on Computer Science of Harvard University, <a href="http://cs.harvard.edu/malan">David Malan</a>'s and Huntington D. Lambert, Dean of Harvard Extension School signature.<br/>
 				<br/> I came from not knowing any coding language, never touching a code editor in my life, to creating for loops, if statements, variables in C, PHP, JavaScript and even making web pages using SQL / CSS and HTML. <br/><br/>
 				CS50 is being offered again late 2014 and 2015 as far as I know. It's a great stepping stone for anyone who wants to get serious with Computer Science.
 				<img  src="img/cs50-certificate.png" alt="CS50 Certificate" />
@@ -47,6 +47,24 @@
 			
 		</div>
 	</div>
+
+	<div class="grid__item one-whole bg-4">
+		<div id="mypartner" class="container top-60">
+			<div class="grid__item one-quarter">
+				<img src="img/gandr2.png" alt="g and r"/>
+			</div>
+			<div class="grid__item two-thirds">
+				<h1>My Partner</h2>
+					<p><span>His name is <a href="http://giolliano.com">Giolliano Sulit</a>, he's my other half.</span><br/><br/>
+						He is a web developer in melbourne. <br/>
+						Yup, I know what you're thinking. He is like the <strong>wizard </strong> and I'm the <strong>apprentice</strong>.<strong> </strong><br/><br/>
+						He is my boyfriend, partner, lover, tutor, and best friend. We've got a <a href="http://giollianoandrachelle.com">blog</a> together, you can read more about us there.<br/><br/>
+						Except from the internet, I leant most of what I know about web development from <em>him</em>.<br/><br/>
+						<strong></strong>
+					</p>
+				</div>
+			</div>
+		</div>
 
 
 
