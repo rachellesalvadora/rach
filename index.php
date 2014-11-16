c8006ba191697a8b07870782d3e35619fa47e663
rachelle <rachellesalvadora@gmail.com>
2014-11-16 22:41:51 +1100

    index.php navigation added

--- index.php
+++ index.php
@@ -7,3 +7,22 @@
 	<title>Rachelle Salvadora</title> 
 	<link rel="stylesheet" href="css/style.css">
 </head>
+
+<body>
+	<div class="grid">
+
+		<div class="grid__item one-whole bg-1" >
+			<div class="wrap top-30">
+
+				<ul class="nav nav--banner nav-top">
+					<li class="nav-top-li"><a href="#cs50">CS50</a></li>
+					<li class="nav-top-li"><a href="#mypartner">My Partner</a></li>
+					<li class="nav-top-li"><a href="#philippines">Philippines Holiday</a></li>
+					<li class="nav-top-li"><a href="#singapore">Singapore Holiday</a></li>
+					<li class="nav-top-li"><a href="#contact">Contact</a></li>
+					<li class="nav-top-li"><em><a href="http://blog.rachellesalvadora.com" target="_blank">My Blog</a></em></li>
+				</ul>
+
+				
+			</div>
+		</div>
